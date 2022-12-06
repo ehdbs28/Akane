360ab937483a3f8e52feb681d0ba2d2e8192b0db
Gusdnd01 <cestop42@naver.com>
2022-12-06 18:58:45 +0900

    플레이어 움직임 및 무기 움직임 구현

---
+++ Assets/01.Scripts/Player/Weapon.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Vector3 followPos;
+    public int followDelay;
+    public Transform parent;
+    public Queue<Vector3> parentPos;
+
+    private void Awake()
+    {
+        parentPos = new Queue<Vector3>();
+    }
+
+    private void Update()
+    {
+        Watch();
+        Movement();
+    }
+
+    private void Watch()
+    {
+        parentPos.Enqueue(parent.position);
+
+        if(parentPos.Count > followDelay)
+            followPos = parentPos.Dequeue();
+    }
+
+    void Movement()
+    {
+        float h = Mathf.Lerp(transform.position.x, followPos.x, 0.5f);
+        float v = Mathf.Lerp(transform.position.y, followPos.y, 0.5f);
+
+        Vector2 moveDir;
+        moveDir = new Vector2(h, v);
+        transform.position = moveDir;
+    }
+}
