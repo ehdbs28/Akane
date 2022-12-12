c3fab82217c2ad7b7fe86320af2a25a5e77edbea
ehdbs28 <doyun5373@naver.com>
2022-12-13 00:50:45 +0900

    Update WhatIsNextSkill.cs

--- Assets/01.Scripts/AI/Decisions/WhatIsNextSkill.cs
+++ Assets/01.Scripts/AI/Decisions/WhatIsNextSkill.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WhatIsNextSkill : AIDecision
 {
     [SerializeField] private int _thisSkillCount;
-
+    
     public override bool MakeADecision()
     {
         return _thisSkillCount == _brain.SkillWave % 4;
