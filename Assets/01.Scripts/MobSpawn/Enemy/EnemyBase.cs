f6c20a225f1ae91d8b1051632f7ec4f535e5d626
Gusdnd01 <cestop42@naver.com>
2022-12-15 20:15:26 +0900

    공격 수정

---
+++ Assets/01.Scripts/MobSpawn/Enemy/EnemyBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : Poolable
+{
+    [SerializeField] private float _speed = 5f;
+
+    public override void Reset()
+    {
+        Debug.Log($"{this.name} : excute method Reset");
+    }
+
+
+    private void Shoot(){
+        transform.position += transform.up * _speed;
+    }
+}
--- Assets/01.Scripts/MobSpawn/MobSpawner.cs
+++ Assets/01.Scripts/MobSpawn/MobSpawner.cs
@@ -1,8 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MobSpawner : MonoBehaviour
 {
-    
+    [SerializeField] private List<Transform> _spawnPos = new List<Transform>();
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private Tilemap tiles;
+
+
+    private float nextStageTime = 5f;
+
+    private void Start() {
+        StartCoroutine(Spawn());
+    }
+
+    [ContextMenu("맵 셋")]
+    private void MapSet(){
+        for(int i = 0; i < tiles.)
+    }
+
+    [ContextMenu("적 셋")]
+    private void EnemySet(){
+        for(int i = 0; i < 5; i++){
+            EnemyBase enemy = PoolManager.Instance.Pop("Enemy") as EnemyBase;
+        }
+    }
+
+    public void SetEnemy(float scale, Vector2 pos,float angle){
+        transform.position = pos;
+        transform.localScale = transform.localScale * scale;
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, angle));
+    }
+
+    IEnumerator Spawn(){
+        while(true){
+            yield return new WaitForSeconds(nextStageTime);
+        }
+    }
 }
